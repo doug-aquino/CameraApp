2cd15c153a7e52c362a5636678dba45dae2722dc
doug-aquino <douglasaquinodev@gmail.com>
2025-08-07 21:53:29 -0300

    Finalizei o layout e comecei a mainactivity

--- app/src/main/java/com/example/cameraapp/MainActivity.kt
+++ app/src/main/java/com/example/cameraapp/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.cameraapp
 
+import android.content.ContentValues
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.ColorMatrix
+import android.graphics.ColorMatrixColorFilter
+import android.graphics.Matrix
+import android.net.Uri
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.SeekBar
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import androidx.core.graphics.createBitmap
+import com.example.photogram.databinding.ActivityMainBinding
 
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var imageView: ImageView
+    private lateinit var btnCamera: Button
+    private lateinit var btnGallery: Button
+    private lateinit var btnFlip: Button
+    private lateinit var btnSave: Button
+    private lateinit var seekBarBrightness: SeekBar
+    private lateinit var seekBarContrast: SeekBar
+    private lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
